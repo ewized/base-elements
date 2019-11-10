44f66e2afaea203ca7d4cc9b082ef4e1ad8909be
Joshua Rodriguez <ewized@gmail.com>
2019-11-10 13:20:49 -0800

    Add image component WIP

---
+++ src/js/components/image.js
@@ -0,0 +1,21 @@
+import { LitElement, html, customElement, property } from 'lit-element'
+import { styles } from '../utils.js'
+import style from '../../scss/image.scss'
+
+const TRANS_PNG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGP6zwAAAgcBApocMXEAAAAASUVORK5CYII=';
+
+@customElement('e-image')
+@styles(style)
+export class Image extends LitElement {
+
+  @property() image = TRANS_PNG
+
+  firstUpdated() {
+    // todo, generate the lazy loading of the image value
+    this.image = this.attributes.source.value
+  }
+
+  render() {
+    return html`<img src=${this.image}></img>`
+  }
+}
