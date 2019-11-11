98baadae745d676c8f6137a6e7b516f4df3b3933
Joshua Rodriguez <ewized@gmail.com>
2019-11-11 12:12:45 -0800

    Use flex-box components as layouts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Components
 export { Paper } from './js/components/paper.js'
 export { Card } from './js/components/card.js'
 export { Link } from './js/components/link.js'
@@ -9,3 +10,6 @@ export { Slideshow } from './js/components/slideshow.js'
 export { Code } from './js/components/code.js'
 export { Icon } from './js/components/icon.js'
 export { Highlight } from './js/components/highlight.js'
+
+// Layout Components
+export { FlexBox, FlexItem, FlexCenter } from './js/layouts/flex.js'
