b4b827df7628cf365bb1c126efaa10243c301f99
Joshua Rodriguez <ewized@gmail.com>
2019-11-11 07:49:37 -0800

    Have the styles decorator support CSSResults

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,11 +1,11 @@
-import { css, unsafeCSS } from 'lit-element'
+import { css, unsafeCSS, CSSResult } from 'lit-element'
 
 
 /** Takes the import object for scss files and inject it into the custom element */
 export const _styles = style => css`${unsafeCSS(style[0][1])}`
 
 /** Creates a decorator for the class */
-export function styles(style) {
+export function styles(...styles) {
   // this is the equalivant of doing in the class
   // static styles = _styles(style)
   return descriptor => {
@@ -16,7 +16,7 @@ export function styles(style) {
         placement: 'static',
         key: 'styles',
         descriptor: {},
-        initializer: () => _styles(style)
+        initializer: () => styles.map(style => style instanceof CSSResult ? style : _styles(style))
       }]
     }
   }
