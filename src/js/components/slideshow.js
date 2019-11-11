4cb21a09f90dd04c67f6ffb263de72bda02d85b3
Joshua Rodriguez <ewized@gmail.com>
2019-11-10 16:32:00 -0800

    Add play/pause features

--- src/js/components/slideshow.js
+++ src/js/components/slideshow.js
@@ -10,24 +10,35 @@ export class Slideshow extends LitElement {
   index = 0
   length = this.children?.length || 0
   @property() image = this.children[0].cloneNode()
+  @property() pause = false
 
   async next() {
-    if (++this.index >= this.length) {
-      this.index = 0
-    }
-    this.image = this.children[this.index].cloneNode()
+    this.item(++this.index >= this.length ? 0 : this.index)
   }
 
   async prev() {
-    if (--this.index <= -1) {
-      this.index = this.length - 1
+    this.item(--this.index <= -1 ? this.length - 1 : this.index)
+  }
+
+  item(i) {
+    if (i < 0 || i >= this.length) {
+      i = 0
     }
+    this.index = i
     this.image = this.children[this.index].cloneNode()
+  }
+
+  async togglePause() {
+    this.pause = !this.pause
   }
 
   firstUpdated() {
     let delay = Number(this.attributes?.delay?.value) || DEFAULT_DELAY
-    this.intervalId = setInterval(async () => this.next(), delay)
+    this.intervalId = setInterval(async () => {
+      if (!this.pause) {
+        this.next()
+      }
+    }, delay)
   }
 
   disconnectedCallback() {
@@ -41,9 +52,18 @@ export class Slideshow extends LitElement {
       <div class="slideshow" style=${this.attributes?.style?.value}>
         <content>
           <div class="image">${this.image}</div>
-          <div class="items">${[ ...Array(this.length) ].map((e, i) => html`<div class="item ${i == this.index ? 'active' : ''}"></div>`)}</div>
-          <div class="control left" @click=${this.prev}></div>
-          <div class="control right" @click=${this.next}></div>
+          <nav class=${this.attributes?.hideControls ? 'hide' : 'show'}>
+            <div class="items">
+              ${[ ...Array(this.length) ].map((e, i) => {
+                if (i === this.index) {
+                  return html`<div class=${this.pause ? 'item pause' : 'item play'} @click=${this.togglePause}></div>`
+                }
+                return html`<div class='item' @click=${() => this.item(i)}></div>`
+              })}
+            </div>
+            <div class="control left" @click=${this.prev}></div>
+            <div class="control right" @click=${this.next}></div>
+          </nav>
         </content>
       </div>
     `
