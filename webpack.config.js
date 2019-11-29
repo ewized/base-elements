8f5a885635ba0b525de529f52e26c1df1630ed63
Joshua Rodriguez <ewized@gmail.com>
2019-11-29 12:56:30 -0800

    Seperate source map and upload build artifact

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   entry: {
     base_elements: './src/index.js',
   },
