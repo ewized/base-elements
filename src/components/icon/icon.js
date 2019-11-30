561e86ca051c3fa48c40ea06d452d9d285ed988d
Joshua Rodriguez <ewized@gmail.com>
2019-11-29 16:35:31 -0800

    Wrap release url with cors-anywhere and use cdn for font-awesome in docs site

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,6 +1,8 @@
-import { LitElement, html, css, customElement } from 'lit-element'
+import { LitElement, html, css, unsafeCSS, customElement } from 'lit-element'
 import { styles } from '../../utils.js'
 import style from './icon.scss'
+
+const BASE_ELEMENTS_FONT_PREFIX = unsafeCSS(window.BASE_ELEMENTS_FONT_PREFIX || 'fonts')
 
 @customElement('e-icon')
 @styles(style)
@@ -13,12 +15,12 @@ export default class Icon extends LitElement {
         font-family: "FontAwesome";
         font-weight: normal;
         font-style: normal;
-        src: url("/assets/fontawesome-webfont.eot");
-        src: url("/assets/fontawesome-webfont.eot?#iefix") format("embedded-opentype"),
-             url("/assets/fontawesome-webfont.woff2") format("woff2"),
-             url("/assets/fontawesome-webfont.woff") format("woff"),
-             url("/assets/fontawesome-webfont.ttf") format("truetype"),
-             url("/assets/fontawesome-webfont.svg#fontawesomeregular") format("svg");
+        src: url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.eot");
+        src: url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.eot?#iefix") format("embedded-opentype"),
+             url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.woff2") format("woff2"),
+             url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.woff") format("woff"),
+             url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.ttf") format("truetype"),
+             url("${BASE_ELEMENTS_FONT_PREFIX}/fontawesome-webfont.svg#fontawesomeregular") format("svg");
       }
     `.styleSheet
     document.adoptedStyleSheets = [ ...document.adoptedStyleSheets, fontFaceSheet ]
