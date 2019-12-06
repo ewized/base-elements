45764b5cf174610f0b49671c36ac86b72281c9f3
Joshua Rodriguez <ewized@gmail.com>
2019-12-05 18:08:27 -0800

    Style markdown elements

--- src/components/markdown/markdown.ts
+++ src/components/markdown/markdown.ts
@@ -1,9 +1,11 @@
 import { LitElement, html, customElement } from 'lit-element'
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
 import { Converter } from 'showdown'
-
+import { styles } from '../../utils'
+import style from './markdown.scss'
 
 @customElement('e-markdown')
+@styles(style)
 export default class Markdown extends LitElement {
   static converter = new Converter({
     smartIndentationFix: true,
